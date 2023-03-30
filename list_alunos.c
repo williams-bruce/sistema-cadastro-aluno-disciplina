32dcd05d49f9b26dc4d74dbcdaa3ddfa57d0aa36
williams-bruce <williamsbruceprep@gmail.com>
2023-03-30 14:16:15 -0300

    Introduzi função de query por cpf

--- list_alunos.c
+++ list_alunos.c
@@ -218,6 +218,28 @@ int query_aluno_by_name(LIST_TYPE *lista, char *nome) {
     return -1;
 }
 
+int query_aluno_by_cpf(LIST_TYPE *lista, char *cpf) {
+    NO *aux = lista->start;
+    if (aux == NULL) {
+        return -1;
+    }
+    for (int i = 0; i < lista->len; i++) {
+        char name_temp[50];
+        char name_test[50];
+        strcpy(name_temp, strlwr(cpf));
+        strcpy(name_test, strlwr((aux->aluno).cpf));
+
+        int teste = strcmp(name_temp, name_test);
+        if (!(teste)) {
+            return i;
+        }
+        if (aux->next != NULL) {
+            aux = aux->next;
+        }
+    }
+    return -1;
+}
+
 
 #undef DATA_TYPE
 #undef NO
